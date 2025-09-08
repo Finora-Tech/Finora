63a60655e7ac8aeababce7ea4792473208fcf5d3
doplinBlue <soeunsense7@gmail.com>
2025-09-08 23:42:12 +0900

    Reapply "chore: gitignore 추가, 백앤드 관련 세팅"
    This reverts commit 793ee1ae91375fa90ce5192313c9a859e74ee085.

---
+++ core-api/src/test/java/com/finora/core/CoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finora.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
